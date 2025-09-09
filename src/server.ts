295fe4253bc9c748154cdcc902e96989af2e3732
josephmatheus <josephmatheus@hotmail.com>
2025-09-09 20:04:40 -0300

    :sparkles: feat: Rota de listagem de produtos

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,29 @@
 import express from "express";
+import { PrismaClient } from "@prisma/client";
 
 const port = 3000;
 const app = express();
+const prisma = new PrismaClient();
 
-app.get("/products", (req, res) => {
-  res.send("Listagem de produtos");
+app.get("/products", async (_, res) => {
+  try {
+    const products = await prisma.product.findMany({
+      orderBy: {
+        id: "asc",
+      },
+      include: {
+        categories: true,
+        brands: true
+      }
+    })
+    res.status(200).json(products);
+  } catch (err) {
+    res.status(500).json({
+      error: "INTERNAL_SERVER_ERROR",
+      message: "Não foi possivel obter os produtos.",
+      timestamp: new Date().toLocaleString()
+    });
+  }
 });
 
 app.listen(port, () => {
