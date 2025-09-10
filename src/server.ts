cc41fa31c8c623f8d60333e07deca5d54748a529
josephmatheus <josephmatheus@hotmail.com>
2025-09-10 19:10:53 -0300

    :sparkles: feat: Incluir imagens na listagem de produtos

--- src/server.ts
+++ src/server.ts
@@ -12,11 +12,29 @@ app.get("/products", async (_, res) => {
         id: "asc",
       },
       include: {
-        categories: true,
-        brands: true
+        categories: {
+          select: {
+            name: true
+          }
+        },
+        brands: {
+          select: {
+            name: true
+          }
+        },
+        products_images: {
+          orderBy: {
+            display_order: "asc"
+          }
+        }
       }
     })
-    res.status(200).json(products);
+    res.status(200).json({
+      "success": true,
+      "message": "Produtos obtidos com sucesso.",
+      "total": products.length,
+      "data": products
+    });
   } catch (err) {
     res.status(500).json({
       error: "INTERNAL_SERVER_ERROR",
