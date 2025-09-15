29706be59db02d18462614246bd357c8f53e0def
josephmatheus <josephmatheus@hotmail.com>
2025-09-15 10:55:00 -0300

    :sparkles: feat: Busca de produtos por ID e filtro de produtos por categoria

--- src/server.ts
+++ src/server.ts
@@ -12,34 +12,111 @@ app.get("/products", async (_, res) => {
         id: "asc",
       },
       include: {
-        categories: {
-          select: {
-            name: true
-          }
-        },
-        brands: {
-          select: {
-            name: true
-          }
-        },
+        categories: true,
+        brands: true,
         products_images: {
           orderBy: {
-            display_order: "asc"
-          }
-        }
-      }
-    })
+            display_order: "asc",
+          },
+        },
+      },
+    });
     res.status(200).json({
-      "success": true,
-      "message": "Produtos obtidos com sucesso.",
-      "total": products.length,
-      "data": products
+      success: true,
+      message: "Produtos obtidos com sucesso.",
+      total: products.length,
+      data: products,
     });
   } catch (err) {
     res.status(500).json({
       error: "INTERNAL_SERVER_ERROR",
       message: "Não foi possivel obter os produtos.",
-      timestamp: new Date().toLocaleString()
+      timestamp: new Date().toLocaleString(),
+    });
+  }
+});
+
+app.get("/products/:id", async (req, res) => {
+  try {
+    const searchID = Number(req.params.id);
+    const product = await prisma.product.findUnique({
+      where: {
+        id: searchID,
+      },
+      include: {
+        categories: true,
+        brands: true,
+        products_images: {
+          orderBy: {
+            display_order: "asc",
+          },
+        },
+      },
+    });
+    if (!product) {
+      return res.status(404).json({
+        error: "NOT_FOUND",
+        message: "Não foi encontrado produto com o ID informado.",
+        timestamp: new Date().toLocaleString(),
+      });
+    }
+    res.status(200).json({
+      success: true,
+      message: "Produto obtido com sucesso.",
+      data: product,
+    });
+  } catch (error) {
+    res.status(500).json({
+      error: "INTERNAL_SERVER_ERROR",
+      message: "Não foi possivel obter o produto.",
+      timestamp: new Date().toLocaleString(),
+    });
+  }
+});
+
+app.get("/category/:name", async (req, res) => {
+  try {
+    const categoryName = req.params.name;
+    const products = await prisma.product.findMany({
+      where: {
+        categories: {
+          name: {
+            equals: categoryName,
+            mode: "insensitive",
+          }
+        },
+      },
+      orderBy: {
+        id: "asc",
+      },
+      include: {
+        categories: true,
+        brands: true,
+        products_images: {
+          orderBy: {
+            display_order: "asc",
+          },
+        },
+      },
+    });
+    if (!products) {
+      return res.status(404).json({
+        error: "NOT_FOUND",
+        message: "Não foi encontrado produto com a categoria informada.",
+        timestamp: new Date().toLocaleString(),
+      });
+    }
+    res.status(200).send({
+      success: true,
+      message: "Produtos obtidos com sucesso.",
+      total: products.length,
+      data: products,
+    });
+  } catch (error) {
+    res.status(500).json({
+      error: "INTERNAL_SERVER_ERROR",
+      message: "Não foi possivel obter os produtos.",
+      timestamp: new Date().toLocaleString(),
     });
   }
 });
